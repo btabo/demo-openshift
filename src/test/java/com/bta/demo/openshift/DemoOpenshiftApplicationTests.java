c88efa989dae375a3647edbf695399af22b16008
Bertrand <bertrand.taboulet@gmail.com>
2020-09-08 21:27:51 +0200

    init project

---
+++ src/test/java/com/bta/demo/openshift/DemoOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bta.demo.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
